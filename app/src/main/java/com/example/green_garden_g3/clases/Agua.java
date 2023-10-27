b51da4df516fae3807350ba9600a03244f36f3d6
mdazag <41237965+mdazag@users.noreply.github.com>
2023-10-27 09:13:42 -0500

    Comentarios

--- app/src/main/java/com/example/green_garden_g3/clases/Agua.java
+++ app/src/main/java/com/example/green_garden_g3/clases/Agua.java
@@ -1,10 +1,14 @@
 package com.example.green_garden_g3.clases;
 
+import java.lang.reflect.Constructor;
+
 public class Agua extends Categoria {
+    // Constructor
     public Agua(String nombre, int costo, String unidad) {
         super(nombre, costo, unidad);
     }
 
+    // Métodos
     @Override
     public double costoConsumo(double cantidad) {
         System.out.println("Calculando consumo de agua");
