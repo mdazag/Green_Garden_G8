aefcf14f9e974c32c740b577c9b505561b0ccb4e
mdazag <41237965+mdazag@users.noreply.github.com>
2023-10-28 15:24:57 -0500

    Se crea la calse DAO para el manejo de registro de datos.

---
+++ app/src/main/java/com/example/green_garden_g3/clases/Dao.java
@@ -0,0 +1,41 @@
+package com.example.green_garden_g3.clases;
+
+import java.util.HashMap;
+
+public class Dao {
+    public HashMap<String, Planta> getPlantas() {
+        HashMap<String, Planta> map = new HashMap<>();
+
+        map.put("Diente de león", new Planta("Diente de león", "Herbáceas"));
+        map.put("Lechuga", new Planta("Lechuga", "Hortalizas"));
+        map.put("Manzanilla", new Planta("Manzanilla", "Herbáceas"));
+        map.put("Papa", new Planta("Papa", "Tubérculos"));
+        map.put("Perejil", new Planta("Perejil", "Verduras"));
+        map.put("Repollo", new Planta("Repollo", "Verduras"));
+        map.put("Tomate", new Planta("Tomate", "Hortalizas"));
+        map.put("Zanahoria", new Planta("Zanahoria", "Tubérculos"));
+
+        return map;
+    }
+
+    public HashMap<String, Categoria> getCatergorias () {
+        HashMap<String, Categoria> map = new HashMap<>();
+
+        map.put("Agua", new Agua("Agua", 100, "lt"));
+        map.put("Abono", new Abono("Abono", 150, "gr"));
+        map.put("Energía", new Energia("Energía", 500, "kWh"));
+
+        return map;
+    }
+
+    public HashMap<Integer, Registro> getRegistros() {
+        HashMap<Integer, Registro> map = new HashMap<>();
+
+        return  map;
+    }
+
+    public boolean putRegistro(Registro registro) {
+
+        return true;
+    }
+}
--- app/src/main/java/com/example/green_garden_g3/clases/Registro.java
+++ app/src/main/java/com/example/green_garden_g3/clases/Registro.java
@@ -7,4 +7,43 @@ public class Registro {
     String planta;
     String categoria;
     double cantidad;
+
+    public Registro(Date fechaConsumo, String planta, String categoria, double cantidad) {
+        this.fechaConsumo = fechaConsumo;
+        this.planta = planta;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+    }
+
+    public Date getFechaConsumo() {
+        return fechaConsumo;
+    }
+
+    public void setFechaConsumo(Date fechaConsumo) {
+        this.fechaConsumo = fechaConsumo;
+    }
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
 }
